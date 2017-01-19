59a0bb038d19ac1f825f84601517ec166128b8cf
Eddie Kao <eddie@digik.com.tw>
2017-01-19 10:46:38 +0800

    add show

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,8 +4,30 @@ class CandidatesController < ApplicationController
     @candidates = Candidate.all
   end
 
+  def show
+    @candidate = Candidate.find_by(id: params[:id])
+    redirect_to candidates_path, notice: "查無資料" unless @candidate
+  end
+
   def new
     @candidate = Candidate.new
+  end
+
+  def edit
+    @candidate = Candidate.find_by(id: params[:id])
+    redirect_to candidates_path, notice: "查無資料" unless @candidate
+  end
+
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+    redirect_to candidates_path, notice: "查無資料" unless @candidate
+    clean_params = params.require(:candidate).permit(:name, :age, :gender, :party)
+
+    if @candidate.update(clean_params)
+      redirect_to candidates_path, notice: "更新成功"
+    else
+      render :edit
+    end
   end
 
   def create
@@ -19,5 +41,13 @@ class CandidatesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    redirect_to candidates_path, notice: "查無資料" unless @candidate
+
+    @candidate.destroy
+    redirect_to candidates_path, notice: "刪除成功"
   end
 end
